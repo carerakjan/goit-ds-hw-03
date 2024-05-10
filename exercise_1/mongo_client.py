98b291b62c502c1958c46dd8829f3f61b1ba50e5
Bo_Hr <carerakjan@gmail.com>
2024-05-10 10:56:14 +0300

    added certifi to fix SSL issue

--- exercise_1/mongo_client.py
+++ exercise_1/mongo_client.py
@@ -1,6 +1,7 @@
+import certifi
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
-connection_string = ""
+connection_string = "mongodb+srv://carerakjan:12345@goit.n8hjpvu.mongodb.net/"
 
-client = MongoClient(connection_string, server_api=ServerApi("1"))
+client = MongoClient(connection_string, server_api=ServerApi("1"), tlsCAFile=certifi.where())
